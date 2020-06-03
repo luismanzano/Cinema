6091c8b36e2c357eb70fe3a2da2d2902350607dd
= <luisfmanzanoa@gmail.com>
2020-06-03 13:13:32 -0400

    Anadido el primer modelo (pelicula)

--- lairAdmin/models.py
+++ lairAdmin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title   = models.CharField(max_length=200)
+    duration = models.DurationField()
+    cast  = models.TextField()
+    director = models.CharField(max_length=200)
+    genre   = models.CharField(max_length=200)
+    language   = models.CharField(max_length=200)
+    #thumb   = models.ImageField(upload_to = os.path.join(BASE_DIR))
+    mode = models.IntegerField(default = 0)
+    showing = models.BooleanField(default = False)
